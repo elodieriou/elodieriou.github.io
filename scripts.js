3a06a79d0f8bfa18a5df1d24fcd4d10bf9ca8e98
Elodie Riou <elodie.riou@hotmail.com>
2023-03-05 15:19:12 +0100

    FEATURE: implement section situation

--- scripts.js
+++ scripts.js
@@ -16,7 +16,4 @@ $(function () {
     changeIconBurgerMenu();
     const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
     const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
-    const popover = new bootstrap.Popover('.popover-dismiss', {
-        trigger: 'focus'
-    })
 })
