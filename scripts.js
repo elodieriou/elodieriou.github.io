50d8ab91ff30424ef86544f0e65faa61c47c1d66
Elodie Riou <elodie.riou@hotmail.com>
2023-03-15 21:21:46 +0100

    FEATURE: implement mes realisations section

--- scripts.js
+++ scripts.js
@@ -16,4 +16,6 @@ $(function () {
     changeIconBurgerMenu();
     const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
     const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 })
