b261cb2223d0492f4eb69e321f5cfe081924c4a7
Elodie Riou <elodie.riou@hotmail.com>
2023-03-27 21:52:25 +0200

    FIX: modifications suites retours utilisateurs

--- scripts.js
+++ scripts.js
@@ -12,10 +12,13 @@ function changeIconBurgerMenu() {
     });
 }
 
-$(function () {
-    changeIconBurgerMenu();
-    const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
-    const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+function displayTooltip() {
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+}
+
+
+$(function () {
+    changeIconBurgerMenu();
+    displayTooltip();
 })
