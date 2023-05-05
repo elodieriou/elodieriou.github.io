029c44987e42c36a4ac7e258a5fd1077c2fa15f7
Elodie Riou <elodie.riou@hotmail.com>
2023-05-05 23:24:01 +0200

    FEATURE: implement dynamic timeline

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+// index.html
 function changeIconBurgerMenu() {
     const element = "#burger-icon i"
     $('#burger-btn').click(function () {
@@ -17,8 +18,39 @@ function displayTooltip() {
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 }
 
+// history.html
+function hideHistory(allHistoryId) {
+    $(allHistoryId).removeClass('d-block');
+    $(allHistoryId).addClass('d-none');
+}
+
+function showHistory(historyId) {
+    $(historyId).removeClass('d-none');
+    $(historyId).addClass('d-block');
+}
+
+function activatedButton(buttonId, historyId) {
+    $(buttonId).addClass('activate');
+    showHistory(historyId);
+}
+
+function desactivateButton(allButtonId, allHistoryId) {
+    $(allButtonId).removeClass('activate');
+    hideHistory(allHistoryId);
+}
+
+function onClickTimeline(allButtonId, allHistoryId) {
+    $(allButtonId).click((event) => {
+        const buttonId = `#${event.target.id}`;
+        const historyId = `#history-${buttonId.split('-')[1]}`;
+        desactivateButton(allButtonId, allHistoryId);
+        activatedButton(buttonId, historyId);
+    });
+}
+
 
 $(function () {
     changeIconBurgerMenu();
     displayTooltip();
-})
+    onClickTimeline('[id^="date-"]', '[id^="history-"]');
+});
